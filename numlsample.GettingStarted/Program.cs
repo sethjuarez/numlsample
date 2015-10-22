1d1addfa179c72d269d61450f8c167f259635771
William Raiford <bill.mybiz@gmail.com>
2015-10-22 16:48:18 -0500

    Initial .GettingStarted project created
    * Recreates current numl.Net Getting Started code.
    * Since this is all GettingStarted code, this code was refactored as
    "QuickStart" code.
    * Tweaked other refactorings for clarity and extended sample
    architecture.
    * Implemented SimpleNumlWorkflow with multiple algorithm examples.
    * I am not sure if all of these algorithms actually make sense to use
    with the Tennis data.
    * Added some pseudo-random Tennis data generation possibilities that are
    enabled/disabled by toggling comments.
    * This is in the SampleData.cs file.
    * I'd like comments on the inferences made from using this data. I have
    added my thoughts in the code itself.

---
+++ numlsample.GettingStarted/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using numlsample.GettingStarted.ExampleCode;
+
+namespace numlsample.GettingStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Comment/uncomment these to get the basics
+
+            // Start here.
+            //QuickStart.Go();
+
+            // These examples use the same simple workflow with numl
+            SimpleNumlWorkflow.Go();
+        }
+    }
+}
